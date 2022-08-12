4ecc7426281f0a747ed17dffe9dfe7ad704d6a87
Martin Bonnin <martin@mbonnin.net>
2022-08-12 13:43:06 +0200

    switch to MavenCentral

--- kotlinx-nodejs/build.gradle.kts
+++ kotlinx-nodejs/build.gradle.kts
@@ -1,11 +1,7 @@
-import com.jfrog.bintray.gradle.BintrayExtension
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
-import org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact
-
 plugins {
     id("org.jetbrains.kotlin.js")
     `maven-publish`
-    id("com.jfrog.bintray") version "1.8.4"
+    id("signing")
 }
 
 group = "org.jetbrains.kotlinx"
@@ -32,53 +28,68 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile>().configureEa
     }
 }
 
-
 val sourcesJar by tasks.registering(Jar::class) {
     classifier = "sources"
     from(kotlin.sourceSets["main"].kotlin.srcDirs)
 }
 
 publishing {
-    publications.invoke {
+    publications {
         register("maven", MavenPublication::class) {
             from(components["kotlin"])
             artifact(sourcesJar.get())
+
+            pom {
+                // Project name missing, Project description missing, Project URL missing, License information missing, SCM URL missing, Developer information missing
+                name.set("kotlinx.nodejs")
+                description.set("Kotlin nodejs API")
+                url.set("https://github.com/Kotlin/kotlinx-nodejs")
+                licenses {
+                    license {
+                        url.set("https://github.com/Kotlin/kotlinx-nodejs/blob/master/LICENSE")
+                        name.set("Apache-2.0")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/Kotlin/kotlinx-nodejs")
+                }
+                developers {
+                    developer {
+                        name.set("kotlin")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "ossSnapshots"
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = System.getenv("SONATYPE_USER")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+
+        maven {
+            name = "ossStaging"
+            setUrl {
+                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
+            credentials {
+                username = System.getenv("SONATYPE_USER")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
         }
     }
 }
 
-tasks.withType<BintrayUploadTask> {
-    doFirst {
-        publishing.publications
-                .filterIsInstance<MavenPublication>()
-                .forEach { publication ->
-                    val moduleFile = buildDir.resolve("publications/${publication.name}/module.json")
-                    if (moduleFile.exists()) {
-                        publication.artifact(object : FileBasedMavenArtifact(moduleFile) {
-                            override fun getDefaultExtension() = "module"
-                        })
-                    }
-                }
-    }
+signing {
+    // GPG_PRIVATE_KEY should contain the armoured private key that starts with -----BEGIN PGP PRIVATE KEY BLOCK-----
+    // It can be obtained with gpg --armour --export-secret-keys KEY_ID
+    useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PRIVATE_KEY_PASSWORD"))
+    sign(publishing.publications)
 }
-
-bintray {
-    user = System.getenv("BINTRAY_USER")
-    key = System.getenv("BINTRAY_API_KEY")
-    publish = true
-    setPublications("maven")
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "kotlinx"
-        name = "kotlinx.nodejs"
-        userOrg = "kotlin"
-        setLicenses("Apache-2.0")
-        githubRepo = "Kotlin/kotlinx-nodejs"
-        websiteUrl = "https://github.com/Kotlin/kotlinx-nodejs"
-        issueTrackerUrl = "https://github.com/Kotlin/kotlinx-nodejs/issues"
-        vcsUrl = "https://github.com/Kotlin/kotlinx-nodejs.git"
-        description = "Kotlin nodejs API"
-        setLabels("kotlin", "kotlinjs", "nodejs", "kotlinx")
-        desc = description
-    })
+tasks.withType(Sign::class.java).configureEach {
+    isEnabled = !System.getenv("GPG_PRIVATE_KEY").isNullOrBlank()
 }
-
